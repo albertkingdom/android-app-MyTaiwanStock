4774f223e8f31e46b210985ed41674e2279427a4
Albert <albertkingdom@gmail.com>
2022-07-31 23:44:53 +0800

    fix NetworkUtils.kt for checking internet status logic

--- app/src/main/java/com/example/mynewsapp/util/NetworkUtils.kt
+++ app/src/main/java/com/example/mynewsapp/util/NetworkUtils.kt
@@ -3,8 +3,6 @@ package com.example.mynewsapp.util
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
-import androidx.core.content.ContextCompat.getSystemService
-import com.example.mynewsapp.MyApplication
 
 fun isNetworkAvailable(context: Context): Boolean {
     //Log.d("viewmodel", "isNetworkAvailable")
@@ -12,18 +10,24 @@ fun isNetworkAvailable(context: Context): Boolean {
         context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
     val capabilities =
         connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-    if (capabilities != null) {
-        when {
-            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
-                return true
-            }
-            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
-                return true
-            }
-            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
-                return true
+        if (capabilities != null) {
+            when {
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                    return true
+                }
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                    return true
+                }
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
+                    return true
+                }
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN) -> {
+                    return true
+                }
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI_AWARE) -> {
+                    return true
+                }
             }
         }
-    }
     return false
 }
